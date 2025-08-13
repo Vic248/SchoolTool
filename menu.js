15fd184c3f15389b0815489fdfd7b5dd7337ce71
Vic248 <victorturcotte04@gmail.com>
2025-08-13 09:19:51 -0400

    test

---
+++ menu.js
@@ -0,0 +1,27 @@
+function loadMenu() {
+    fetch("menu.json")
+        .then(response => response.json())
+        .then(menuLinks => {
+            const menuContainer = document.createElement("div");
+            menuContainer.id = "menu";
+
+            menuLinks.forEach(link => {
+                const a = document.createElement("a");
+                a.href = link.href;
+                a.setAttribute("onmouseenter", `mouseEnter('${link.id}')`);
+                a.setAttribute("onmouseleave", `mouseLeave('${link.id}')`);
+
+                const i = document.createElement("i");
+                i.className = link.icon;
+                i.id = link.id;
+
+                a.appendChild(i);
+                a.appendChild(document.createTextNode(" " + link.text));
+
+                menuContainer.appendChild(a);
+            });
+
+            document.body.appendChild(menuContainer);
+        })
+        .catch(error => console.error("Erreur de chargement du menu :", error));
+}
