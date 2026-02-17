2d77a0df17cb1e8c19412e93e8102dd65262f5ee
Vic248 <victorturcotte04@gmail.com>
2026-02-17 15:38:02 -0500

    test

---
+++ First Person Shooter/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2428365.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 276409742, 276409456, 276409442, 276409453, 276409439 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/276409445/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/276409451/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/276409443/1/ammo.js', 'preload' : true},
+];
